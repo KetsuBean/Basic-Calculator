19844a8b6d7f02550b1d2c5f37eabc1c35743173
Kevin Dao <daomkevin@gmail.com>
2020-07-11 13:10:19 -0700

    added cpp file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Basic Calculator
+//
+//  Created by Kevin Dao on 7/11/20.
+//  Copyright © 2020 Kevin Dao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
