9f09e794deb710cc35344f540b5be89141fc3b84
Kevin Dao <daomkevin@gmail.com>
2020-07-11 13:47:53 -0700

    code for calculations

--- main.cpp
+++ main.cpp
@@ -1,15 +1,91 @@
-//
-//  main.cpp
 //  Basic Calculator
+//  This program will just take two numbers and preform a basic mathematical function
+//  with them.
 //
-//  Created by Kevin Dao on 7/11/20.
-//  Copyright © 2020 Kevin Dao. All rights reserved.
-//
+//  07/11/2020
+//  Kevin Dao
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int additionCalc(int, int);
+int subtractionCalc(int, int);
+int divisionCalc(int, int);
+int multiplicationCalc(int, int);
+
+int main() {
+    
+    char operation;
+    int num1, num2;
+    int result;
+    
+    cout << "This program takes two numbers and preforms a basic mathematical function." << endl << endl;
+    cout << "Please enter an operator: (+, -, /, *)" << endl;
+    
+    cin >> operation;
+    
+    cout << endl << "Please enter a number";
+    cin >> num1;
+    
+    cout << endl << "Please enter another number";
+    cin >> num2;
+    
+    switch (operation) {
+        case '+':
+            result = additionCalc(num1, num2);
+        case '-':
+            result = subtractionCalc(num1, num2);
+        case '*':
+            result = multiplicationCalc(num1, num2);
+        case '/':
+            result = divisionCalc(num1, num2);
+            break;
+            
+        default:
+            cout << "An incorrect operation was typed";
+            result = 0;
+            break;
+    }
+    
+    
+    cout << endl << "Result is " << result << endl;
+    
     return 0;
+}
+
+int additionCalc(int n1, int n2)
+{
+    int r;
+    
+    r = n1 + n2;
+    
+    return r;
+}
+
+int subtractionCalc(int n1, int n2)
+{
+    int r;
+    
+    r = n1 - n2;
+    
+    return r;
+}
+
+int multiplicationCalc(int n1, int n2)
+{
+    int r;
+    
+    r = n1 * n2;
+    
+    return r;
+}
+
+int divisionCalc(int n1, int n2)
+{
+    int r;
+    
+    r = n1 / n2;
+    
+    return r;
 }
